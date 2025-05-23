c15d92fe12a8382964502703235fa9a39014abe0
chriss212 <20235787@esen.edu.sv>
2025-05-22 20:38:22 -0600

    Implement customer and reservation modules with CRUD operations and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,26 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReservationsModule } from './reservations/reservations.module';
+import { CustomersModule } from './customers/customers.module';
+import { CustomerEntity } from './customers/customer.entity';
+import { ReservationEntity } from './reservations/reservation.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
+      port: 5433,
       username: 'postgres',
-      password: 'postgres',
-      database: 'actividades',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Set to false in production
+      password: 'suser',
+      database: 'clase6',
+      entities: [CustomerEntity, ReservationEntity],
+      synchronize: true,
+      
     }),
+    CustomersModule, 
+    ReservationsModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/reservations/reservations.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationEntity } from './reservation.entity';
+
+@Controller('reservations')
+export class ReservationsController {
+  constructor(private readonly reservationsService: ReservationsService) {}
+
+  @Post()
+  create(@Body() reservationData: {
+    startDate: Date;
+    endDate: Date;
+    customerId: number;
+  }): Promise<ReservationEntity> {
+    return this.reservationsService.createReservation(reservationData);
+  }
+
+  @Get()
+  findAll(): Promise<ReservationEntity[]> {
+    return this.reservationsService.findAll();
+  }
+
+  @Get('reservation/:reservationId')
+  findOne(@Param('reservationId', ParseIntPipe) id: number): Promise<ReservationEntity> {
+    return this.reservationsService.findOne(id);
+  }
+
+  @Get('customer/:customerId')
+  findByCliente(@Param('customerId', ParseIntPipe) customerId: number): Promise<ReservationEntity[]> {
+    return this.reservationsService.findByCustomer(customerId);
+  }
+}
